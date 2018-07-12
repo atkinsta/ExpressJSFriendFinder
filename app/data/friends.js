2bb2aed797db5fe50e628fd6719df0a90e75cc4b
atkinsta <atkinstaa@gmail.com>
2018-07-12 19:04:56 -0400

    Added contact info box

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,6 +2,7 @@ let friends = [
     {
         "name": "Mathew",
         "photo": "http://api.theweek.com/sites/default/files/author-images/headshottheweek.png?resize=500x500",
+        "Contact": "Idk, probably on twitter.com",
         "scores": [
             "1",
             "1",
@@ -18,6 +19,7 @@ let friends = [
     {
         "name": "Luke",
         "photo": "https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg/revision/latest/scale-to-width-down/500?cb=20170927034529",
+        "contact": "The Force",
         "scores": [
             "3",
             "3",
@@ -34,6 +36,7 @@ let friends = [
     {
         "name": "John",
         "photo": "https://pbs.twimg.com/profile_images/972872769019850753/YTxFZF2x_400x400.jpg",
+        "contact": "linkedin.com",
         "scores": [
             "5",
             "5",
